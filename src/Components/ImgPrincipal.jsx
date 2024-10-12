705966400b6b533d8eaa6e094b043eebddfb55ab
ErickRoman <erickrra11@gmail.com>
2024-10-11 21:32:57 -0600

    PrimerCommit

--- src/Components/ImgPrincipal.jsx
+++ src/Components/ImgPrincipal.jsx
@@ -2,7 +2,7 @@
 export function ImgPrincipal(){
     return <div>
         <img src="https://wallpapers.com/images/featured/fondods-de-atardecer-en-la-montana-e94f78n373kjvxpn.jpg" alt="error" 
-        style={{height:'450px'}} className="w-100 "/>
+        style={{height:'450px'}} className="w-100"/>
     </div>
 }
 
